b5fa34152054821bca63b23a7d93964f7c97bcf6
Max-sterni <maxvonsternberg@gmail.com>
2022-05-30 13:48:38 +0200

    Triangle Aufgabe

--- am/Assignment_10/barycentric_interpolation.jl
+++ am/Assignment_10/barycentric_interpolation.jl
@@ -20,29 +20,30 @@ uv_1 = [[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]];
 # arguments: vertices (v), point (q), colors we'd like to interpolate (uv)
 function barycentric_interpolation(v, q, uv)
     # calculate vectors between vertices and q
-    vectors = zeros(3, 3)
+    vectors = [[0,0,0],[0,0,0],[0,0,0]]
     for i in 1:3
-        vectors[:, i] = v[i, :] - q
+        vectors[i] = v[i] - q
     end
     # get area of each triangle via crossproducts
-    areas = zeros(3)
-    for i in 1:3
-        areas[:, i] = norm(cross(vectors[:, i], vectors[:, i % 3 + 1]) / 2)
-    end
+    areas = [0.0, 0.0, 0.0]
+    areas[1] = norm(cross(vectors[1], vectors[2])) / 2
+    areas[2] = norm(cross(vectors[2], vectors[3])) / 2
+    areas[3] = norm(cross(vectors[1], vectors[3])) / 2
     # get main triangle area
-    surrounding_vectors = [v_1[2, :] - v_1[1, :], v_1[3, :] - v_1[1, :]]
-    area = norm(cross(surrounding_vectors[1], surrounding_vectors[2]) / 2)
+    A = norm(cross(v[2] - v[1], v[3] - v[1])) / 2
     # calculate barycentric coordinates 
     # (with sign for the more general version)
-    lambda = [areas[1] / area, areas[2] / area, areas[3] / area]
+    lambda = [0.0, 0.0, 0.0]
+    lambda[1] = areas[1] / A
+    lambda[2] = areas[2] / A
+    lambda[3] = areas[3] / A
     # apply interpolation factors (barycentric coordinates) if q inside triangle
-    if(all(lambda .< 1 && lambda .> 0))
-        color = lambda[1] * uv[1] + lambda[2] * uv[2] + lambda[3] * uv[3] 
-    else
-        color = [1.0 1.0 1.0]
-    end
     
-    return color
+    if lambda[1] + lambda[2] + lambda[3] ≈ 1
+        return lambda[1] * uv[1] + lambda[2] * uv[2] + lambda[3] * uv[3]
+    end
+    return [1 1 1]
+    # return color for point q
 end
 
 # iterate through points in triangle
@@ -58,7 +59,7 @@ function barycentric_interpolation_triangle(v, uv, x_max, y_max, c_array)
 end
 
 barycentric_interpolation_triangle(v_1, uv_1, window_height, window_width, color);
-
+# barycentric_interpolation(v_1, [200, 256, 0] ,uv_1)
 # convert color array to image
 img = colorview(RGB, color);
 # save image as PNG

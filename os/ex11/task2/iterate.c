cf1867c2ecb6157eedb7637bde16528b30c2e717
Max-sterni <maxvonsternberg@gmail.com>
2022-06-05 17:01:26 +0200

    Fix and refactoring

--- os/ex11/task2/iterate.c
+++ os/ex11/task2/iterate.c
@@ -1,3 +1,3 @@
 int iterate(int n){
-	return n++;
+	return ++n;
 }
--- os/ex11/task2/map_number.c
+++ os/ex11/task2/map_number.c
@@ -6,6 +6,10 @@
 
 #define BUF_SIZE 128
 
+#define ARGUMENT_OFFSET 2
+#define PREFIX_OFFSET 2
+#define SUFFIX_OFFSET 3
+
 int main(int argc, char ** argv){
 	
 	// Argument guard 
@@ -23,11 +27,10 @@ int main(int argc, char ** argv){
 	}
 
 	// Get the handles
-	void * handles[argc - 2];
+	void * handles[argc - ARGUMENT_OFFSET];
 
-	for(int i = 0; i < argc - 2; i++){
-		handles[i] = dlopen(argv[i + 2], RTLD_NOW);
-		printf("%s\n", argv[i+2]);
+	for(int i = 0; i < argc - ARGUMENT_OFFSET; i++){
+		handles[i] = dlopen(argv[i + ARGUMENT_OFFSET], RTLD_NOW);
 		if(handles[i] == NULL){
 			perror("Invalid lib");
 			exit(EXIT_FAILURE);
@@ -37,23 +40,38 @@ int main(int argc, char ** argv){
 	// Get the functions
 	int(*func_pointer)(int);
 	char buffer[BUF_SIZE];	
+	int length = 0;
+	int name_beginning = 0;
 
-	for(int i = 0; i < argc - 2; i++){
+	for(int i = 0; i < argc - ARGUMENT_OFFSET; i++){
 		memset(buffer, 0, BUF_SIZE);
-		strncpy(buffer, argv[i + 2] + 2, strlen(argv[i + 1] + 2) - 3);
+
+		// Find the beginning of function name
+		for(size_t j = 0; j < strlen(argv[i + ARGUMENT_OFFSET]); j++){
+			if(argv[i + ARGUMENT_OFFSET][j] == '/'){
+				name_beginning = j + 1;
+			}
+		}
+
+		// Calculate length of function name
+		length = strlen(argv[i + ARGUMENT_OFFSET] + name_beginning) - SUFFIX_OFFSET;
+
+		// Save function name in buffer
+		strncpy(buffer, argv[i + ARGUMENT_OFFSET] + name_beginning, length);
+
+		// Extract function pointer
 		func_pointer = dlsym(handles[i], buffer);
 		
 		// Error guard
 		if(func_pointer == NULL){
-			perror("Library error");
+			perror("Library loading error");
 			exit(EXIT_FAILURE);
 		}
 
 		n = func_pointer(n);
+		printf("%s: %d\n", argv[i + ARGUMENT_OFFSET], n);
 		
 	}
-
-	printf("%d\n", n);
 	
 	return EXIT_SUCCESS;
 }
